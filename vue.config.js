5085ec5aa1940fcab1088b8c4635b4f46c90968b
shun-harutaro <n.shunshun2004@gmail.com>
2021-05-25 06:44:40 +0900

    fix vue.config

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ module.exports = {
   outputDir: 'docs',
     configureWebpack: {
       performance: {
-        maxAssetSize: 2_097_152,  // 2 MiB
-        maxEntrypointSize: 2_097_152,  // 2 MiB
+        maxAssetSize: 300000,  // 2 MiB
+        maxEntrypointSize: 300000,  // 2 MiB
       },
     },
   };
